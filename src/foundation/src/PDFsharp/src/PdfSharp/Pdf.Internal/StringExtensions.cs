81e6b2334ef07854829a41e81ec0cd534b612f32
Thomas Terhaar <uncletom@gmx.de>
2023-05-09 22:26:45 +0200

    Add StringExtensions (used during AcroField import)

---
+++ src/foundation/src/PDFsharp/src/PdfSharp/Pdf.Internal/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace PdfSharp.Pdf.Internal
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Add a number-suffix to the provided string.<br></br>
+        /// If the string already has a number-suffix, the suffix is incremented by one.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string AddIncrementalSuffix(this string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            var numberSuffix = 1;   // will be incremented to 2 if text does not have a suffix
+            var idx = text.Length - 1;
+            // check if value ends with a number. if it does, increment it
+            while (idx >= 0 && char.IsDigit(text[idx]))
+                idx--;
+            idx++;
+            if (idx < text.Length)
+                numberSuffix = int.Parse(text[idx..]);
+            numberSuffix++;
+            var namePrefix = text[..idx];
+            return namePrefix + numberSuffix.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
