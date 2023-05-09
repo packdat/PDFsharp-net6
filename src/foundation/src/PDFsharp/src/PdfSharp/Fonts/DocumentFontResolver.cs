7ba2ccee3e4360ea468f6496bed073bda9ce6b1a
Thomas Terhaar <uncletom@gmx.de>
2023-05-09 22:07:30 +0200

    Add the 14 Standard-Fonts and a FontResolver that uses them

---
+++ src/foundation/src/PDFsharp/src/PdfSharp/Fonts/DocumentFontResolver.cs
@@ -0,0 +1,140 @@
+﻿using PdfSharp.Fonts.StandardFonts;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.AcroForms;
+using PdfSharp.Pdf.Advanced;
+
+namespace PdfSharp.Fonts
+{
+    /// <summary>
+    /// Resolves standard-fonts and/or fonts in existing documents.<br></br>
+    /// </summary>
+    public class DocumentFontResolver : IFontResolver
+    {
+        private readonly PdfDocument? document;
+        private readonly PdfAcroField? acroField;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="DocumentFontResolver"/>
+        /// </summary>
+        /// <param name="document"></param>
+        public DocumentFontResolver(PdfDocument? document = null)
+        {
+            this.document = document;
+        }
+
+        /// <summary>
+        /// Internal constructor used by <see cref="PdfAcroField"/>
+        /// </summary>
+        /// <param name="field"></param>
+        internal DocumentFontResolver(PdfAcroField field)
+        {
+            document = field.Owner;
+            acroField = field;
+        }
+
+        /// <summary>
+        /// Gets the data for the specified font.
+        /// </summary>
+        /// <param name="faceName">Name of the font</param>
+        /// <returns>Font data or null, if no font with the specified name could be found</returns>
+        public byte[]? GetFont(string faceName)
+        {
+            var data = StandardFontData.GetFontData(faceName);
+            if (data != null)
+            {
+                return data;
+            }
+            if (document == null)
+                return null;
+
+            if (!faceName.StartsWith('/'))
+                faceName = "/" + faceName;
+
+            var possibleResources = new List<PdfDictionary?>
+            {
+                document.AcroForm?.Elements.GetDictionary(PdfAcroForm.Keys.DR),
+                acroField?.Elements.GetDictionary(PdfAcroForm.Keys.DR)
+            };
+            foreach (var page in document.Pages)
+            {
+                possibleResources.Add(page.Resources);
+            }
+            foreach (var resources in possibleResources)
+            {
+                if (resources != null && resources.Elements.ContainsKey("/Font"))
+                {
+                    var fontList = resources.Elements.GetDictionary("/Font");
+                    var fontRef = fontList?.Elements.GetReference(faceName);
+                    if (fontRef != null)
+                    {
+                        var fontDict = fontRef.Value as PdfDictionary;
+                        var descriptor = fontDict?.Elements.GetDictionary(PdfFont.Keys.FontDescriptor);
+                        if (descriptor != null)
+                        {
+                            var fileRef = descriptor.Elements.GetDictionary(PdfFontDescriptor.Keys.FontFile2);
+                            var fontData = fileRef?.Stream.UnfilteredValue;
+                            if (fontData != null)
+                            {
+                                return fontData;
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Get a <see cref="FontResolverInfo"/> for the specified font
+        /// </summary>
+        /// <param name="familyName">Name of the font</param>
+        /// <param name="isBold"></param>
+        /// <param name="isItalic"></param>
+        /// <returns>A <see cref="FontResolverInfo"/> or null, if no font with the specified name could be found</returns>
+        public FontResolverInfo? ResolveTypeface(string familyName, Boolean isBold, Boolean isItalic)
+        {
+            var data = StandardFontData.GetFontData(familyName);
+            if (data != null)
+            {
+                return new FontResolverInfo(familyName, isBold, isItalic);
+            }
+            if (document == null)
+                return null;
+
+            if (!familyName.StartsWith('/'))
+                familyName = "/" + familyName;
+
+            var possibleResources = new List<PdfDictionary?>
+            {
+                document.AcroForm?.Elements.GetDictionary(PdfAcroForm.Keys.DR),
+                acroField?.Elements.GetDictionary(PdfAcroForm.Keys.DR)
+            };
+            foreach (var page in document.Pages)
+            {
+                possibleResources.Add(page.Resources);
+            }
+            foreach (var resources in possibleResources)
+            {
+                if (resources != null && resources.Elements.ContainsKey("/Font"))
+                {
+                    var fontList = resources.Elements.GetDictionary("/Font");
+                    var fontRef = fontList?.Elements.GetReference(familyName);
+                    if (fontRef != null)
+                    {
+                        var fontDict = fontRef.Value as PdfDictionary;
+                        var descriptor = fontDict?.Elements.GetDictionary(PdfFont.Keys.FontDescriptor);
+                        if (descriptor != null)
+                        {
+                            var fileRef = descriptor.Elements.GetDictionary(PdfFontDescriptor.Keys.FontFile2);
+                            if (fileRef != null)
+                            {
+                                return new FontResolverInfo(familyName.TrimStart('/'), isBold, isItalic);
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ src/foundation/src/PDFsharp/src/PdfSharp/Fonts/StandardFonts/StandardFontData.cs
@@ -0,0 +1,64 @@
+﻿namespace PdfSharp.Fonts.StandardFonts
+{
+    /// <summary>
+    /// Provides the data for the 14 Standard-Fonts defined in the PDF-specification.<br></br>
+    /// Pdf spec. 1.7, Chapter 9.6.2.2: Standard Type 1 Fonts (Standard 14 Fonts)
+    /// </summary>
+    public static class StandardFontData
+    {
+        /// <summary>
+        /// Gets the data for the specified font
+        /// </summary>
+        /// <param name="fontName">Name of the font</param>
+        /// <returns>Font-data or null if a font with the specified name could not be found</returns>
+        public static byte[]? GetFontData(string fontName)
+        {
+            if (string.IsNullOrEmpty(fontName))
+                return null;
+
+            // if the name comes from a resource-dictionary...
+            if (fontName.StartsWith('/'))
+                fontName = fontName.TrimStart('/');
+
+            if (fontData.TryGetValue(fontName, out var data))
+                return data;
+            return null;
+        }
+
+        /// <summary>
+        /// Indicates, whether the specified <paramref name="fontName"/> is one of the 14 Standard-Fonts
+        /// </summary>
+        /// <param name="fontName">Font-name to check</param>
+        /// <returns>true, if <paramref name="fontName"/> is one of the 14 Standard-Fonts, otherwise false</returns>
+        public static bool IsStandardFont(string fontName)
+        {
+            return FontNames.Contains(fontName.TrimStart('/'));
+        }
+
+        /// <summary>
+        /// Get the names of the supported standard-fonts
+        /// </summary>
+        public static IEnumerable<string> FontNames => fontData.Keys;        
+
+        private static readonly Dictionary<string, byte[]> fontData = new()
+        {
+            { "Courier", Courier.FontData },
+            { "Courier-Bold", CourierBold.FontData },
+            { "Courier-Oblique", CourierItalic.FontData },
+            { "Courier-BoldOblique", CourierBoldItalic.FontData },
+
+            { "Helvetica", Helvetica.FontData },
+            { "Helvetica-Bold", HelveticaBold.FontData },
+            { "Helvetica-Oblique", HelveticaItalic.FontData },
+            { "Helvetica-BoldOblique", HelveticaBoldItalic.FontData },
+
+            { "Times-Roman", TimesRoman.FontData },
+            { "Times-Bold", TimesRomanBold.FontData },
+            { "Times-Italic", TimesRomanItalic.FontData },
+            { "Times-BoldItalic", TimesRomanBoldItalic.FontData },
+
+            { "ZapfDingbats", ZapfDingbats.FontData },
+            { "Symbol", Symbol.FontData }
+        };
+    }
+}
